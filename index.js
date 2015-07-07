2d5cf0aa15c68d4808565252e421ed2a8cc41ffc
Marcellin Nshimiyimana <mars@fusemachines.com>
2015-07-07 18:20:57 -0400

    use custom addon name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Ember CLI Simple Auth OAuth 2.0',
 
   included: function(app) {
-    app.import(app.bowerDirectory + '/ember-simple-auth/simple-auth-oauth2.amd.js', {
+    app.import(app.bowerDirectory + '/ember-simple-auth-input/simple-auth-oauth2.amd.js', {
       exports: {
         'simple-auth-oauth2/authenticators/oauth2': ['default'],
         'simple-auth-oauth2/authorizers/oauth2':    ['default'],
